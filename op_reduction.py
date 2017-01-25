4b9e0b0ec81e7e6894cf315b178a3c666faa6379
noprom <tyee.noprom@qq.com>
2017-01-25 11:47:42 +0800

    add op reduction

---
+++ op_reduction.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+import tensorflow as tf
+
+sess = tf.InteractiveSession()
+x = tf.constant([[1, 2, 3],
+                 [3, 2, 1],
+                 [-1, -2, -3]])
+
+boolean_tensor = tf.constant([[True, False, True],
+                              [False, False, True],
+                              [True, False, False]])
+
+# reduce prod
+tf.reduce_prod(x, reduction_indices=1).eval()
+# reduce min
+tf.reduce_min(x, reduction_indices=1).eval()
+# reduce max
+tf.reduce_max(x, reduction_indices=1).eval()
+# reduce mean
+tf.reduce_mean(x, reduction_indices=1).eval()
+# reduce all
+tf.reduce_all(boolean_tensor, reduction_indices=1).eval()
+# reduce any
+tf.reduce_any(boolean_tensor, reduction_indices=1).eval()
