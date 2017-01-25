4b260b9aee508485f4ed94b4a94ba6d3d9917503
noprom <tyee.noprom@qq.com>
2017-01-25 10:02:27 +0800

    add op matrix

---
+++ op_matrix.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+
+import tensorflow as tf
+
+sess = tf.InteractiveSession()
+x = tf.constant([[2, 5, 3, -5],
+                 [0, 3, -2, 5],
+                 [4, 3, 5, 3],
+                 [6, 1, 4, 0]])
+y = tf.constant([[4, -7, 4, -3, 4],
+                 [6, 4, -7, 4, 7],
+                 [2, 3, 2, 1, 4],
+                 [1, 5, 5, 5, 2]])
+floatx = tf.constant([[2., 5., 3., -5.],
+                      [0., 3., -2., 5.],
+                      [4., 3., 5., 3.],
+                      [6., 1., 4., 0.]])
+# Transpose matrix
+tf.transpose(x).eval()
+# Matrix multiplication
+tf.matmul(x, y).eval()
+# Matrix determinant
+tf.matrix_determinant(floatx).eval()
+# Matrix inverse
+tf.matrix_inverse(floatx).eval()
+# Solve Matrix system
+tf.matrix_solve(floatx, [[1], [1], [1], [1]]).eval()
